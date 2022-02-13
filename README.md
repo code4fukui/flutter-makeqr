3721feeba5282518b48fd57d18ab911cac2f38c3
Taisuke Fukuno <fukuno@jig.jp>
2022-02-14 06:40:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# makeqr
+
+A new Flutter project to show [QR-Code by DENSO WAVE](https://www.denso-wave.com/ja/adcd/fundamental/2dcode/qrc/index.html).
+
+## How to create this app on Flutter
+
+```
+mkdir makeqr
+flutter create .
+```
+add dependency "[qr_flutter](https://pub.dev/packages/qr_flutter/install): ^4.0.0" in pubspec.yaml
+```
+flutter pub get
+```
+add QrImage and "import 'package:qr_flutter/qr_flutter.dart';" code in main.dart
+```
+flutter run --release
+```
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR counter',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'QR counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            QrImage(
+              data: 'https://fukuno.jig.jp/$_counter',
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+            Text(
+              'https://fukuno.jig.jp/$_counter',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
